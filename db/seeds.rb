02f8356c6d15f709fb436c9722ee2f5e12ed818a
Runefire32 <thomas_g@orange.fr>
2018-10-30 20:17:13 +0100

    apppasterminée

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+10.times do
+	user = User.create!(username: Faker::Name.first_name , email: Faker::Internet.email, bio: Faker::Community.quotes)
+end
